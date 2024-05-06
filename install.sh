8f81bd419cac0d287587c10ab9e3f90b51dc3ffc
Mattia Mascarello <mattia.mascarello@outlook.it>
2024-05-06 09:30:29 +0200

    Updated wording in install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Check if script is run with sudo
 if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run with sudo." 1>&2
+    echo "This script must be run as root." 1>&2
     exit 1
 fi
 
